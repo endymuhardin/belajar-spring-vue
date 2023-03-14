c1100f2b4921d051cc226f37c74c17ebb52669cd
Endy Muhardin <endy.muhardin@gmail.com>
2023-03-14 21:41:52 +0700

    expose as micro frontend

--- spring-backend/src/main/java/com/muhardin/endy/belajar/springvue/config/CorsConfig.java
+++ spring-backend/src/main/java/com/muhardin/endy/belajar/springvue/config/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**");
+                registry.addMapping("/assets/**");
             }
 
         };
--- vue-frontend/vite.config.ts
+++ vue-frontend/vite.config.ts
@@ -1,11 +1,31 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import federation from "@originjs/vite-plugin-federation";
+import topLevelAwait from "vite-plugin-top-level-await";
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(), 
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: "__tla",
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: i => `__tla_${i}`
+    }),
+    federation({
+      name: 'remote-app',
+      filename: 'remoteEntry.js',
+      // Modules to expose
+      exposes: {
+          './UserList': './src/components/Users/UsersList.vue',
+      },
+      shared: ['vue']
+  })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
