a507d4b4e96e124f680aacb2516f17ab980b0cb8
Endy Muhardin <endy.muhardin@gmail.com>
2023-03-15 08:58:05 +0700

    refactor konfigurasi

--- vue-frontend/src/services/user_management.ts
+++ vue-frontend/src/services/user_management.ts
@@ -1,10 +1,9 @@
 export class UserManagementService {
-
-    private urlBase = "http://localhost:8080";
+    
     private urlUserList = "/api/user/";
 
     public async findUsers() : Promise<Page<User>> {
-        return fetch(this.urlBase + this.urlUserList)
+        return fetch(import.meta.env.VITE_BACKEND_URL + this.urlUserList)
                 .then(response => Promise.resolve(response.json()))
     }
 }
